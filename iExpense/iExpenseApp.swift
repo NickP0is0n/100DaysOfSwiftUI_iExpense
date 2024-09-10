b76253743f86bdc66b32adeabf1e4154a229ab43
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-10 11:07:16 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mykola Chaikovskyi on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
